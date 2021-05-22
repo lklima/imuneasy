387f7d85fec39f779537c59c7772db7d42aef0db
Danilo Machado de Sousa <danilomsou@gmail.com>
2021-05-22 05:45:20 -0300

    chore: add global styles

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
+      <GlobalStyles />
       <h1>Hello ImunEasy</h1>    
     </div>
   );
